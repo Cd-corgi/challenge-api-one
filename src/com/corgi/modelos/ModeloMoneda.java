78d3bfe12aba91b3233885f75b2e08ac26c641bf
syste <systemjpc@hotmail.com>
2024-10-05 12:24:09 -0500

    Revert "para challenge"
    This reverts commit 59641f5eee2fd6d5568afce2878d799122433dd8.

--- src/com/corgi/modelos/ModeloMoneda.java
+++
@@ -1,7 +0,0 @@
-package com.corgi.modelos;
-
-public record ModeloMoneda(String base_code,
-                           String target_code,
-                           double conversion_rate,
-                           double conversion_result) {
-}
